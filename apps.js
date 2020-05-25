0fc12b233555cb47f4641f6f70e8349735f2433a
Cristian Berrios <equistene@gmail.com>
2020-05-25 14:59:34 -0400

    minor advances

--- apps.js
+++ apps.js
@@ -1,47 +1,43 @@
-'use strict'
-
-function getFlour(){
-
+function getFlour() {
   let formDough = document.getElementById("formDough");
   let flour = document.getElementById("flour").value;
 
-  let water = flour * 0.70;
-  let salt = flour * 0.02;
-  let yeast = flour * 0.02;
+  let water = Math.round(flour * 0.7);
+  let salt = Math.round(flour * 0.02);
+  let yeast = Math.round(flour * 0.02);
 
-  if(flour > 2000){
-    console.log('¿Tanta masa vas a hacer?');
+  if (flour > 2000) {
+    console.log("¿Tanta masa vas a hacer?");
   }
 
-  var div = document.getElementById('app');
+  var div = document.getElementById("app");
 
   div.innerHTML += `
   <div class="content">  
     <p class="big">Si usas <strong> ${flour} grs</strong> de harina necesitas:</p>
     <p><strong>${water}cc</strong> de agua</p>
     <p><strong>${salt}grs</strong> de sal</p>
-    <p><strong>${yeast}grs</strong> de levadura</p>
+    <p><strong>${yeast}grs</strong> de levadura seca</p>
 
     <button id="recalculate" onClick="window.location.reload();">Volver a calcular</button>
   </div>
   `;
-  
-  console.log('Si usas ' + flour + 'grs de harina necesitas:');
-  console.log('Agua: ' + water + 'cc de agua');
-  console.log('Sal: ' + salt + 'grs de sal');
-  console.log('Levadura: ' + yeast + 'grs de levadura');
+
+  console.log("Si usas " + flour + "grs de harina necesitas:");
+  console.log("Agua: " + water + "cc de agua");
+  console.log("Sal: " + salt + "grs de sal");
+  console.log("Levadura: " + yeast + "grs de levadura");
 
   formDough.remove();
-
 }
 
 // let flour = document.getElementById("flour").value;
-// 
+//
 // function calculoMasa() {
 //   const water = flour * 0.70;
 //   const salt = flour * 0.02;
 //   const yeast = flour * 0.02;
-  
+
 //   console.log('Agua: ' + water + 'cc de agua');
 //   console.log('Sal: ' + salt + 'grs de sal');
 //   console.log('Levadura: ' + yeast + 'grs de levadura');
@@ -50,4 +46,3 @@ function getFlour(){
 // calculoMasa();
 
 // console.log('Si usas ' + flour + 'grs de harina necesitas:');
-
